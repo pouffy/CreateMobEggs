a7265202dfef3139569d33071866c64e7733d783
8KV <82761117+8KV@users.noreply.github.com>
2023-11-18 21:07:54 +0200

    updates

--- src/main/java/com/example/modid/mixin/MinecraftMixin.java
+++ src/main/java/com/example/modid/mixin/MinecraftMixin.java
@@ -1,20 +1,69 @@
 package com.example.modid.mixin;
 
 import com.example.modid.ExampleMod;
+import com.simibubi.create.content.kinetics.crusher.CrushingWheelControllerBlock;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.world.effect.MobEffect;
 
-import net.minecraft.client.main.GameConfig;
+import net.minecraft.world.effect.MobEffects;
+
+import net.minecraft.world.entity.MobType;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
+import com.simibubi.create.content.kinetics.crusher.CrushingWheelControllerBlockEntity;
+import com.simibubi.create.foundation.utility.VecHelper;
+
+import net.minecraft.core.Direction;
+import net.minecraft.core.Registry;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Mixin(CrushingWheelControllerBlockEntity.class)
 public class MinecraftMixin {
-	@Inject(method = "<init>", at = @At("TAIL"))
-	private void example$init(GameConfig gameConfig, CallbackInfo ci) {
-		ExampleMod.LOGGER.info("Hello from {}", ExampleMod.NAME);
+
+	@Shadow(remap = false)
+	public Entity processingEntity;
+	@Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;setPos(DDD)V", shift = At.Shift.AFTER))
+	private void injected(CallbackInfo ci) {
+		if(!processingEntity.isAlive() && processingEntity instanceof LivingEntity){
+
+			boolean hasRegeneration = ((LivingEntity) processingEntity).hasEffect(MobEffects.REGENERATION);
+			boolean hasWeakness = ((LivingEntity) processingEntity).hasEffect(MobEffects.WEAKNESS);
+
+
+			boolean hasEffect = ((LivingEntity) processingEntity).getMobType() == MobType.UNDEAD ? hasWeakness : hasRegeneration;
+
+			if(hasEffect){
+				var self = (CrushingWheelControllerBlockEntity)(Object)this;
+				Vec3 centerPos = VecHelper.getCenterOf(self.getBlockPos());
+				Direction facing = self.getBlockState().getValue(CrushingWheelControllerBlock.FACING);
+				int offset = facing.getAxisDirection().getStep();
+				String entityNamespace = EntityType.getKey(processingEntity.getType()).getNamespace();
+				String entityId = EntityType.getKey(processingEntity.getType()).toString().replace(entityNamespace + ":", "");
+				ResourceLocation eggLocation = new ResourceLocation(entityNamespace, entityId + "_spawn_egg");
+				Vec3 outSpeed = new Vec3((facing.getAxis() == Direction.Axis.X ? 0.25D : 0.0D) * offset, offset == 1 ? (facing.getAxis() == Direction.Axis.Y ? 0.5D : 0.0D) : 0.0D, (facing.getAxis() == Direction.Axis.Z ? 0.25D : 0.0D) * offset);
+				Vec3 outPos = centerPos.add((facing.getAxis() == Direction.Axis.X ? .55f * offset : 0f), (facing.getAxis() == Direction.Axis.Y ? .55f * offset : 0f), (facing.getAxis() == Direction.Axis.Z ? .55f * offset : 0f));
+				ItemStack eggStack = Registry.ITEM.get(eggLocation).getDefaultInstance();
+				ItemEntity entitySpawnEgg = new ItemEntity(processingEntity.level, outPos.x, outPos.y, outPos.z, eggStack);
+				entitySpawnEgg.setDeltaMovement(outSpeed);
+				entitySpawnEgg.getExtraCustomData().put("BypassCrushingWheel", NbtUtils.writeBlockPos(self.getBlockPos()));
+				processingEntity.level.addFreshEntity(entitySpawnEgg);
+			}
+
+		}
 	}
 }
